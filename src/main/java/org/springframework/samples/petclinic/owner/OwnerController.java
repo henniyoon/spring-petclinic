540514d3213b05bcf2328c466e669a9087959b1c
henniyoon <2749126@naver.com>
2024-08-31 21:47:17 +0900

    feat: Owner 검색 옵션 last name에서 first name으로 변경

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -91,15 +91,15 @@ class OwnerController {
 	public String processFindForm(@RequestParam(defaultValue = "1") int page, Owner owner, BindingResult result,
 			Model model) {
 		// allow parameterless GET request for /owners to return all records
-		if (owner.getLastName() == null) {
-			owner.setLastName(""); // empty string signifies broadest possible search
+		if (owner.getFirstName() == null) {
+			owner.setFirstName(""); // empty string signifies broadest possible search
 		}
 
-		// find owners by last name
-		Page<Owner> ownersResults = findPaginatedForOwnersLastName(page, owner.getLastName());
+		// find owners by first name
+		Page<Owner> ownersResults = findPaginatedForOwnersFirsttName(page, owner.getFirstName());
 		if (ownersResults.isEmpty()) {
 			// no owners found
-			result.rejectValue("lastName", "notFound", "not found");
+			result.rejectValue("firstName", "notFound", "not found");
 			return "owners/findOwners";
 		}
 
@@ -122,11 +122,17 @@ class OwnerController {
 		return "owners/ownersList";
 	}
 
-	private Page<Owner> findPaginatedForOwnersLastName(int page, String lastname) {
+	private Page<Owner> findPaginatedForOwnersFirsttName(int page, String firstName) {
 		int pageSize = 5;
 		Pageable pageable = PageRequest.of(page - 1, pageSize);
-		return owners.findByLastName(lastname, pageable);
+		return owners.findByFirstName(firstName, pageable);
 	}
+
+//	private Page<Owner> findPaginatedForOwnersLastName(int page, String lastname) {
+//		int pageSize = 5;
+//		Pageable pageable = PageRequest.of(page - 1, pageSize);
+//		return owners.findByLastName(lastname, pageable);
+//	}
 
 	@GetMapping("/owners/{ownerId}/edit")
 	public String initUpdateOwnerForm(@PathVariable("ownerId") int ownerId, Model model) {
